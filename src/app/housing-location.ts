05fae04a424b8b42e0b88bece5bb3fe1fa82a7d7
theeugenechong <eugenechong20011@gmail.com>
2022-05-26 18:18:43 +0800

    Finish app

---
+++ src/app/housing-location.ts
@@ -0,0 +1,10 @@
+// used to "shape" the data in TypeScript
+export interface HousingLocation {
+    name: string,
+    city: string,
+    state: string,
+    photo: string,
+    availableUnits: number,
+    wifi: boolean,
+    laundry: boolean
+}
