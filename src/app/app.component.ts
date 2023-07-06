74872c4906b438f214a3001af40066f8d7677952
John Beveridge <john.beveridge@fcc-fac.ca>
2023-07-06 12:20:42 -0600

    ready to commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fairhouse';
+  housingLocationList: HousingLocation[] = [
+    {
+      name: "Acme Fresh Start Housing",
+      city: "Chicago",
+      state: "IL",
+      photo: "../assets/housing-1.jpg",
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "A113 Transitional Housing",
+      city: "Santa Monica",
+      state: "CA",
+      photo: "../assets/housing-2.jpg",
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      name: "Warm Beds Housing Support",
+      city: "Juneau",
+      state: "AK",
+      photo: "../assets/housing-3.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    }
+  ];
 }
