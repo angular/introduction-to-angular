76aa1ba135e3a51a3920feb4b820a564012a0272
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2024-03-12 11:51:01 +0100

    Generate componenet housing-list

---
+++ src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css']
+})
+export class HousingListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
