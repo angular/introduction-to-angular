6148f7269bc1ecfbf98d031c8551bd97c0e129c7
Mandla <127042808+th0Rn4Do@users.noreply.github.com>
2024-03-12 20:34:39 +0100

    Import housing location interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'fairhouse';
